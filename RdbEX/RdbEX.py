d9502b5ac324a8a9a7da0eb6da84c7455555d24f
Kokonico <96935064+Kokonico@users.noreply.github.com>
2023-10-03 03:25:00 +0000

    more commands

--- RdbEX/RdbEX.py
+++ RdbEX/RdbEX.py
@@ -1,35 +1,65 @@
+import importlib
+import os
+
 from replit import db
 
 from RdbEX import config
 
+# internal variables
+
+methods = [
+  "FROM",
+  "SECRET",
+  "FILE"
+]
+
+
+ban = ["#", " ", "[", "]", "{", "}", "(", ")", config.sep] + config.banned_characters
+
+msg = config.msg + "#"
 
 # internal functions
+def splice_msg(path):
+  splitted = path.split("#")
+  return splitted[len(splitted) - 1]
+  
+def valcheck(value):
+  for i in value:
+    if i in ban:
+      raise ValueError("character not allowed: " + i)
+  return False
+
 def root(inpath):
-  newpath = inpath
+  newpath = splice_msg(inpath)
   if inpath[0] != config.sep:
     newpath = config.sep + inpath
   if newpath[len(newpath) - 1] != config.sep:
     newpath = newpath + config.sep
   return newpath.split(config.sep)
+  
 # public functions
     
 def create(key: str, value, path: str = config.sep):
-  inpath = config.sep.join(root(path))
+  valcheck(key)
+  valcheck(root(path))
+  inpath = msg + config.sep.join(root(path))
   db[inpath + key] = value
 
 def delete(key: str, path: str = config.sep):
-  inpath = config.sep.join(root(path))
+  valcheck(key)
+  valcheck(root(path))
+  inpath = msg + config.sep.join(root(path))
   del db[inpath + key]
   
 def list(path: str = config.sep, removeprefix: bool = True, sortbydirs: bool = False):
   "list all keys within a path"
   prefix = config.sep.join(root(path))
-  selected_keys = db.prefix(prefix)
+  selected_keys = db.prefix(msg + prefix)
   result_list = []
   intermediate=[]
   for i in selected_keys:
     if removeprefix:
-      intermediate.append(i.removeprefix(prefix))
+      intermediate.append(i.removeprefix(msg + prefix))
     else:
       intermediate.append(i)
 
@@ -47,6 +77,36 @@ def list(path: str = config.sep, removeprefix: bool = True, sortbydirs: bool = F
 
 def drop(path: str = config.sep):
   "drop all values within a path"
+  valcheck(root(path))
   
   for i in list(path, False):
     del db[i]
+
+def reference(method, input):
+  mu = method.upper()
+  ref = ""
+  header = config.ref + mu + " "
+  if mu not in methods:
+    raise ValueError("bad method")
+  if mu == "FROM":
+    ref = header + config.sep.join(root(input))[:-1]
+  
+  elif mu == "SECRET":
+    ref = header + input
+  return ref
+def read(key: str, path: str = config.sep):
+    inpath = config.sep.join(root(path))
+    value = db[msg + inpath + key]
+    if isinstance(value, str):
+        if value[:3] == config.ref:
+            value = value[3:]
+            cmd = value.split()[0]
+            if cmd == "FROM":
+                vpath = config.sep.join(value.split(config.sep)[:-1]).split()[1]
+                vkey = value.split(config.sep)[-1]
+                return read(vkey, vpath)
+            elif cmd == "SECRET":
+              return os.environ[value.split()[-1]]
+            
+    else:
+        return value
